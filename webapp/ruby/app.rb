0e57ff39d351fd793028dc287d31b53cca7ff31f
kona <kitami.kitano20@gmail.com>
2023-11-24 19:42:50 +0900

    postsテーブルからusersテーブルのN+1の解消

--- webapp/ruby/app.rb
+++ webapp/ruby/app.rb
@@ -101,7 +101,9 @@ module Isuconp
 
       def make_posts(results, all_comments: false)
         posts = []
+        # すべての投稿をループ
         results.to_a.each do |post|
+          # コメントの件数取得
           post[:comment_count] = db.prepare('SELECT COUNT(*) AS `count` FROM `comments` WHERE `post_id` = ?').execute(
             post[:id]
           ).first[:count]
@@ -110,6 +112,7 @@ module Isuconp
           unless all_comments
             query += ' LIMIT 3'
           end
+          # postに紐づくコメントをせんぶ取得
           comments = db.prepare(query).execute(
             post[:id]
           ).to_a
@@ -120,12 +123,11 @@ module Isuconp
           end
           post[:comments] = comments.reverse
 
-          post[:user] = db.prepare('SELECT * FROM `users` WHERE `id` = ?').execute(
-            post[:user_id]
-          ).first
+          post[:user] = {
+            account_name: post[:account_name],
+          }
 
-          posts.push(post) if post[:user][:del_flg] == 0
-          break if posts.length >= POSTS_PER_PAGE
+          posts.push(post)
         end
 
         posts
@@ -225,7 +227,7 @@ module Isuconp
     get '/' do
       me = get_session_user()
 
-      results = db.query('SELECT `id`, `user_id`, `body`, `created_at`, `mime` FROM `posts` ORDER BY `created_at` DESC')
+      results = db.query("SELECT `p.id`, `p.user_id`, `p.body`, `p.created_at`, `p.mime`, `u.account_name` FROM `posts` AS p JOIN `users` AS u ON p.user_id = u.id WHERE u.del_flg = 0 ORDER BY `p.created_at` DESC #{POSTS_PER_PAGE}")
       posts = make_posts(results)
 
       erb :index, layout: :layout, locals: { posts: posts, me: me }
@@ -240,7 +242,7 @@ module Isuconp
         return 404
       end
 
-      results = db.prepare('SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `user_id` = ? ORDER BY `created_at` DESC').execute(
+      results = db.prepare("SELECT `p.id`, `p.user_id`, `p.body`, `p.created_at`, `p.mime`, `u.account_name` FROM `posts` AS p JOIN `users` AS u ON p.user_id = u.id WHERE u.del_flg = 0 AND `u.user_id` = ? ORDER BY `p.created_at` DESC LIMIT #{POSTS_PER_PAGE}").execute(
         user[:id]
       )
       posts = make_posts(results)
@@ -269,7 +271,7 @@ module Isuconp
 
     get '/posts' do
       max_created_at = params['max_created_at']
-      results = db.prepare('SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `created_at` <= ? ORDER BY `created_at` DESC').execute(
+      results = db.prepare("SELECT `p.id`, `p.user_id`, `p.body`, `p.created_at`, `p.mime`, `u.account_name` FROM `posts` AS p JOIN `users` AS u ON p.user_id = u.id WHERE u.del_flg = 0 AND `p.created_at` <= ? ORDER BY `p.created_at` DESC LIMIT #{POSTS_PER_PAGE}").execute(
         max_created_at.nil? ? nil : Time.iso8601(max_created_at).localtime
       )
       posts = make_posts(results)
@@ -278,7 +280,7 @@ module Isuconp
     end
 
     get '/posts/:id' do
-      results = db.prepare('SELECT * FROM `posts` WHERE `id` = ?').execute(
+      results = db.prepare("SELECT `p.id`, `p.user_id`, `p.body`, `p.created_at`, `p.mime`, `u.account_name` FROM `posts` AS p JOIN `users` AS u ON p.user_id = u.id WHERE u.del_flg = 0 AND `p.id` = ? ORDER BY `p.created_at` DESC LIMIT #{POSTS_PER_PAGE}").execute(
         params[:id]
       )
       posts = make_posts(results, all_comments: true)
